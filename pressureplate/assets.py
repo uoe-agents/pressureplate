ca6b5127557974ae9cd685afeb7ad6d13dc89c54
trevormcinroe <tf.research.main@gmail.com>
2021-11-25 10:21:31 -0600

    updating assets dict

--- pressureplate/assets.py
+++ pressureplate/assets.py
@@ -23,12 +23,6 @@ LINEAR = {
 		[8, 11],
 
 		# Second wall
-		# [7, 9],
-		# [7, 10],
-		# [7, 11],
-		# [7, 12],
-		# [7, 13],
-		# [7, 14],
 		[0, 7],
 		[1, 7],
 		[4, 7],
@@ -38,12 +32,6 @@ LINEAR = {
 		[8, 7],
 
 		# Third wall
-		# [3, 9],
-		# # [3, 10],
-		# [3, 11],
-		# [3, 12],
-		# [3, 13],
-		# [3, 14]
 		[0, 3],
 		[1, 3],
 		[2, 3],
@@ -76,83 +64,5 @@ LINEAR = {
 
 	'FOUR_PLAYER_GOAL': [
 		[3, 1]
-	]
-}
-
-FOUR_CORNERS = {
-	'FOUR_PLAYER_WALLS': [
-		# Top left room
-		[3, 0],
-		[3, 1],
-		[3, 2],
-		[3, 3],
-		[0, 3],
-		[1, 3],
-
-		# Top right room
-		[0, 8],
-		[1, 8],
-		[2, 8],
-		[3, 8],
-		[3, 10],
-		[3, 11],
-
-		# Bottom left room
-		[8, 0],
-		[8, 1],
-		[8, 3],
-		[9, 3],
-		[10, 3],
-		[11, 3],
-
-		# Bottom right room
-		[8, 8],
-		[8, 9],
-		[8, 10],
-		[8, 11],
-		[9, 8],
-		[10, 8]
-	],
-
-	'FOUR_PLAYER_DOORS': [
-		# Top left room (bottom left plate opens)
-		[2, 3],
-
-		# Top right room (top left plate opens)
-		[3, 9],
-
-		# Bottom right room (top right plate opens)
-		[11, 8]
-	],
-
-	'FOUR_PLAYER_PLATES': [
-		# Bottom left plate (1)
-		[10, 1],
-
-		# Top left plate (2)
-		[1, 1],
-
-		# Top right plate (3)
-		[1, 10],
-
-	],
-
-	'FOUR_PLAYER_AGENTS': [
-		# Agent 1 (bottom left plate)
-		[7, 4],
-
-		# Agent 2 (top left plate)
-		[4, 4],
-
-		# Agent 3 (top right plate)
-		[4, 7],
-
-		# Agent 4 (bottom right goal)
-		[7, 7]
-	],
-
-	'FOUR_PLAYER_GOAL': [
-		# Bottom right room
-		[10, 10]
 	]
 }
